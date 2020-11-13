1f06f5299efaf07e8bfe9585f14facff7dac62ff
aahnik <66209958+aahnik@users.noreply.github.com>
2020-11-13 06:12:43 +0530

    script to publish a package to pypi

---
+++ pypi.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+python3 -m pip install --upgrade pip
+python3 -m pip install setuptools wheel twine
+cd $1
+rm -rf __pycache__
+python3 setup.py sdist bdist_wheel
+twine upload dist/*
+rm -rf build dist *.egg-info
